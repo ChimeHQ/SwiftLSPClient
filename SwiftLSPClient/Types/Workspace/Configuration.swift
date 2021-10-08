2f3019c45944efddbe364b521a678a61ce743324
Matt <85322+mattmassicotte@users.noreply.github.com>
2021-10-08 10:59:28 -0400

    Public init for Configuration

--- SwiftLSPClient/Types/Workspace/Configuration.swift
+++ SwiftLSPClient/Types/Workspace/Configuration.swift
@@ -3,8 +3,17 @@ import Foundation
 public struct ConfigurationItem: Codable {
     public var scopeUri: DocumentUri?
     public var section: String?
+
+    public init(scopeUri: DocumentUri?, section: String?) {
+        self.scopeUri = scopeUri
+        self.section = section
+    }
 }
 
 public struct ConfigurationParams: Codable {
     public var items: [ConfigurationItem]
+
+    public init(items: [ConfigurationItem]) {
+        self.items = items
+    }
 }
